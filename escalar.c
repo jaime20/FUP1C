430a0a810d702bc4dbaca229a6fada2335366595
jaime <jaimeantonio_lopezperez@hotmail.com>
2015-12-09 22:05:47 -0600

    matrices

---
+++ escalar.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+int matriz[3][3];
+int i,j,Filas,Columnas,n1,resul;
+int main()
+{
+	for(i=0;i<3;i++)
+	{
+		for (j=0;j<3;j++)
+		{
+		printf("Matriz [%d][%d]\n",i+1,j+1);
+		scanf("%d", &matriz[i][j]);
+		}
+	}
+	for(i=0;i<3;i++)
+	{
+		for(j=0;j<3;j++)
+		{
+			printf("%d ", matriz[i][j]);
+		}printf("\n");
+	}
+	printf("Dame el escalar\n");
+	scanf("%d",&n1);
+		for(i=0;i<3;i++)
+	{
+		for (j=0;j<3;j++)
+		{
+		resul=matriz[i][j]*n1;
+		printf("%d ", resul);
+		}printf("\n");
+	}
+	system("PAUSE");
+	return 0;
+}
